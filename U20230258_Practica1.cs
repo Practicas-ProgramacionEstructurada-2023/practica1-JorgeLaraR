796ffe9a538698b1f7e2bada3b93ced2476f4771
Your Name <you@example.com>
2023-08-12 16:24:09 -0600

    Practica1

---
+++ U20230258_Practica1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MyApp// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+            //Declaracion de variables
+            string? nombre;
+            int edad;
+            string? direccion;
+            int telefono;
+            string? universidad;
+            string? carrera;
+            string? facultad;
+            int ciclo;
+
+            Console.WriteLine("\n*******************Practica 1 - Programacion estructurada*******************\n");
+
+            Console.WriteLine("Ingrese su nombre completo:");
+            nombre = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su edad:");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su direccion:");
+            direccion = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su telefono:");
+            telefono = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su universidad:");
+            universidad = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su carrera:");
+            carrera = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su facultad:");
+            facultad = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su ciclo:");
+            ciclo = Convert.ToInt32(Console.ReadLine());
+
+
+            //Imprimiendo datos
+            Console.WriteLine("Su nombre es: " + nombre);
+            Console.WriteLine("Su edad es: " + edad);
+            Console.WriteLine("Su direccion es: " + direccion);
+            Console.WriteLine("Su telefono es: " + telefono);
+            Console.WriteLine("Su universidad es: " + universidad);
+            Console.WriteLine("Su carrera es: " + carrera);
+            Console.WriteLine("Su facultad es: " + facultad);
+            Console.WriteLine("Su ciclo es: " + ciclo);
+        }
+    }
+}
